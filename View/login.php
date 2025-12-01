0fe8b0cea694d69846fefc6d5d0f941d9ba20f6a
Bilal <bilelchaybouti@gmail.com>
2025-12-01 12:13:56 +0100

    création de la view pour login

---
+++ View/login.php
@@ -0,0 +1,28 @@
+<?php require_once("header.php");?>
+<form action='login.php' method="POST">
+    <?php if (isset($errors["general"])): ?>
+        <div style="color: red"><?= htmlspecialchars($errors["general"]) ?></div>
+    <?php endif ?>
+
+    <h1>Connexion</h1>
+
+    <div>
+        <label for="email">Email (requis) :</label>
+        <input type="email" name="email" id="email" value="<?= htmlspecialchars($email) ?>" required />
+        <?php if (isset($errors["email"])): ?>
+            <p style="color: red"><?= htmlspecialchars($errors["email"]) ?></p>
+        <?php endif ?>
+    </div>
+
+    <div>
+        <label for="password">Mot de passe (requis) :</label>
+        <input type="password" name="password" id="password" required />
+        <?php if (isset($errors["password"])): ?>
+            <p style="color: red"><?= htmlspecialchars($errors["password"]) ?></p>
+        <?php endif ?>
+    </div>
+
+    <button type="submit">Se connecter</button>
+</form>
+
+<?php require_once("footer.php"); ?>
