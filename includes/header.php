fd57e0e5507f9b8eaa4b81ebe49be3c709045938
Ilyes <i.chouiter@ecole-ipssi.net>
2025-12-01 11:25:02 +0100

    ajout function_user

--- includes/header.php
+++ includes/header.php
@@ -1,30 +1,51 @@
 <?php
-// Démarre la session si nécessaire
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Inclure la configuration et fonctions
 require_once("./config/config.php");
 require_once("./includes/functions.php");
 
 ?>
 
-<link rel="stylesheet" href="assets/css/styles.css">
 
-<nav>
-    <div class="nav-container">
-        <!-- Menu -->
-        <ul class="header-menu"> 
-            <li><a href="./index.php">Accueil</a></li>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>   
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
 
-            <?php if (isset($_SESSION["user_id"])): ?>
-                <li><a href="./dashboard.php" >Dashboard</a></li>
-                <li><a href="./profile.php" >Profil</a></li>
-                <li><a href="./logout.php">Déconnexion</a></li>
-            <?php else: ?>
-                <li><a href="./login.php" >Connexion</a></li>
-            <?php endif; ?>
+<header>
+    <nav>
+        <div>
+            <ul>
+                <li><a href="./index.php">Accueil</a></li>
+
+                <?php if (isset($_SESSION["user_id"])): ?>
+                    <?php if ($_SESSION["role"] === 'admin'): ?>
+                        <li><a href="./admin.php">Espace Admin</a></li>
+                    <?php elseif ($_SESSION["role"] === 'ecole'): ?>
+                        <li><a href="./ecole.php">Espace École</a></li>
+                    <?php elseif ($_SESSION["role"] === 'entreprise'): ?>
+                        <li><a href="./entreprise.php">Espace Entreprise</a></li>
+                    <?php else: ?>
+                        <li><a href="./dashboard.php">Dashboard</a></li>
+                    <?php endif; ?>
+
+                    <li><a href="./profile.php">Profil</a></li>
+                    <li><a href="./logout.php">Déconnexion</a></li>
+
+                <?php else: ?>
+                    <li><a href="./login.php">Connexion</a></li>
+                <?php endif; ?>
+                
             </ul>
-    </div>
-</nav>
+        </div>
+    </nav>
+</header>
+
