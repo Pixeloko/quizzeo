f36a82b2b407ca6057ac88248f09c9b8d87d70ba
Bilal <bilelchaybouti@gmail.com>
2025-12-01 12:45:12 +0100

    changement entreprise et user

---
+++ View/user.php
@@ -0,0 +1,16 @@
+<?php require_once ('header.php')?>
+
+<main>
+<h1>Dashboard Utilisateur</h1>
+
+<h2> Bonjour <?= htmlspecialchars($username) ?> </h2>
+
+
+<h2>Les quizz auquels vous avez répondu</h2>
+
+<div>
+    
+
+
+</div>
+</main>
