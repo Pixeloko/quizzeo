e71bdb45b11acc155904ce02e84ac00dfe88dfa7
Pixeloko <lola-louna@outlook.fr>
2025-12-01 12:49:03 +0100

    creation de questions

--- View/question.php
+++ View/question.php
@@ -1,14 +1,14 @@
 <?php require_once("header.php"); ?>
 
 <main>
-    <h1>Créer un article</h1>
+    <h1>Créer une nouvelle question </h1>
 
     <form method="POST">
         <?php if (isset($errors["general"])):?>
         <div style="color:red;"><?= $errors["general"];?></div>
         <?php endif ?>
         <div>
-            <label for="title">Titre (requis) :</label>
+            <label for="title">La question :</label>
             <input type="text" name="title" id="title" value="<?= htmlspecialchars($title) ?>">
             <?php if (isset($errors["title"])): ?>
             <p style="color: red;"><?= $errors["title"] ?></p>
@@ -16,15 +16,13 @@
         </div>
 
         <div>
-            <label for="content">Contenu :</label>
-            <textarea name="content" id="content" rows="10" cols="50"><?= htmlspecialchars($content) ?>
-      </textarea>
+            <label for="answer">Réponse :</label>
+            <input name="answer" id="answer" value="<?= htmlspecialchars($answer) ?>">
         </div>
 
         <div>
-            <label for="published">Publié : </label>
-            <input type="checkbox" name="published" id="published" <?php $published ? "checked" : ""
-            // Permet de retourner un bool avec ce qui est entré ?> />
+            <label for="point">Points : </label>
+            <input type="number" name="point" id="point" value="<?php $point?>">
         </div>
 
         <button>Créer</button>
