3c367628687bb3a02fe991848bfe40a25d728c47
Ilyes <i.chouiter@ecole-ipssi.net>
2025-12-01 11:54:49 +0100

    changement index

--- index.php
+++ index.php
@@ -2,17 +2,10 @@
 declare(strict_types=1);
 require_once("./includes/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'accueil</title>
-</head>
-<body>
+
+<main>
     <h1>Site de création de quizz</h1>
+</main>
     
-</body>
-</html>
 
 <?php require_once("./includes/footer.php");?>
