d9e457d7235ffd61c9de0603faafa86356c201d2
Pixeloko <lola-louna@outlook.fr>
2025-12-01 11:10:45 +0100

    ajout gitignore et index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+require_once("./includes/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <h1>Site de création de quizz</h1>
+    
+</body>
+</html>
+
+<?php require_once("./includes/footer.php");?>
