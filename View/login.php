7084f1e7e6418f9147fd20597040eadcb921960c
Ilyes <i.chouiter@ecole-ipssi.net>
2025-12-01 12:53:12 +0100

    mise a jour login

--- View/login.php
+++ View/login.php
@@ -1,28 +1,34 @@
-<?php require_once("header.php");?>
-<form action='login.php' method="POST">
-    <?php if (isset($errors["general"])): ?>
-        <div style="color: red"><?= htmlspecialchars($errors["general"]) ?></div>
-    <?php endif ?>
+<?php 
+    require_once("header.php");
+    require_once("../Model/function_user.php")
+?>
 
-    <h1>Connexion</h1>
-
-    <div>
-        <label for="email">Email (requis) :</label>
-        <input type="email" name="email" id="email" value="<?= htmlspecialchars($email) ?>" required />
-        <?php if (isset($errors["email"])): ?>
-            <p style="color: red"><?= htmlspecialchars($errors["email"]) ?></p>
+<main>
+    <form action='login.php' method="POST">
+        <?php if (isset($errors["general"])): ?>
+            <div style="color: red"><?= htmlspecialchars($errors["general"]) ?></div>
         <?php endif ?>
-    </div>
 
-    <div>
-        <label for="password">Mot de passe (requis) :</label>
-        <input type="password" name="password" id="password" required />
-        <?php if (isset($errors["password"])): ?>
-            <p style="color: red"><?= htmlspecialchars($errors["password"]) ?></p>
-        <?php endif ?>
-    </div>
+        <h1>Connexion</h1>
 
-    <button type="submit">Se connecter</button>
-</form>
+        <div>
+            <label for="email">Email (requis) :</label>
+            <input type="email" name="email" id="email" value="<?= htmlspecialchars($email) ?>" required />
+            <?php if (isset($errors["email"])): ?>
+                <p style="color: red"><?= htmlspecialchars($errors["email"]) ?></p>
+            <?php endif ?>
+        </div>
+
+        <div>
+            <label for="password">Mot de passe (requis) :</label>
+            <input type="password" name="password" id="password" required />
+            <?php if (isset($errors["password"])): ?>
+                <p style="color: red"><?= htmlspecialchars($errors["password"]) ?></p>
+            <?php endif ?>
+        </div>
+
+        <button type="submit">Se connecter</button>
+    </form>
+</main>
 
 <?php require_once("footer.php"); ?>
