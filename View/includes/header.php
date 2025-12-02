5d93d03a5e2611b5fa102d56b3d27029bce22ca5
Pixeloko <lola-louna@outlook.fr>
2025-12-02 22:57:46 +0100

    set up admin dasboard and ativate/desactive user but to be seen : the redirections (good night)

--- View/includes/header.php
+++ View/includes/header.php
@@ -4,6 +4,11 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+
 // Inclure la configuration si nécessaire
 require_once __DIR__ . '/../../config/config.php';
 
