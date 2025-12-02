ff4515170f062b62a8a9da155e6558a7813cfe84
Bilal <bilelchaybouti@gmail.com>
2025-12-02 10:34:37 +0100

    login fixed

--- View/login.php
+++ View/login.php
@@ -10,9 +10,7 @@
         <?php if (isset($errors["general"])): ?>
             <div style="color: red"><?= htmlspecialchars($errors["general"]) ?></div>
         <?php endif ?>
-
         <h1>Connexion</h1>
-
         <div>
             <label for="email">Email (requis) :</label>
             <input type="email" name="email" id="email" value="<?= htmlspecialchars($email) ?>" required />
@@ -20,7 +18,6 @@
                 <p style="color: red"><?= htmlspecialchars($errors["email"]) ?></p>
             <?php endif ?>
         </div>
-
         <div>
             <label for="password">Mot de passe (requis) :</label>
             <input type="password" name="password" id="password" required />
@@ -34,4 +31,4 @@
     <a href="View/create_account.php">S'inscrire</a>
 </main>
 
-<?php require_once("footer.php"); ?>
+<?php require_once("View/includes/footer.php"); ?>
