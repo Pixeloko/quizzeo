5fee4c22f2f03ed9eab5cbe1b44c7930b01fccce
Bilal <bilelchaybouti@gmail.com>
2025-12-03 15:42:16 +0100

    activer/desactiver

---
+++ Controller/activate_account.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../Model/function_user.php";
+ 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+ 
+if ($_SESSION["role"] !== "admin") {
+    header("Location: logout.php");
+    exit;
+}
+ 
+$userId = $_SESSION["user_id"];
+ 
+try {
+    $activate =  setActiveUser($userId);
+    $_SESSION["message"] = "✅ Utilisateur activé";
+    header("Location: ../View/admin.php");
+    exit;
+} catch (Exception $e) {
+    $_SESSION["message"] = "❌ Erreur : " . $e->getMessage();
+    header("Location: dashboard.php");
+    exit;
+}
