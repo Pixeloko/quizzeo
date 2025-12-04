3abe929b14d11a1dced09f95b65dfff20f418f2a
Bilal <bilelchaybouti@gmail.com>
2025-12-04 10:21:24 +0100

    redirection lola nul

--- Controller/login.php
+++ Controller/login.php
@@ -38,7 +38,7 @@
             if ($user["role"] === "admin") {
                 header("Location: ../View/admin.php");
             } elseif ($user["role"] === "ecole" || $user["role"] === "entreprise") {
-                header("Location: /View/ecole/dashboard.php");
+                header("Location: /quizzeo/ecole");
             } else {
                 header("Location: ../View/user.php");
             }
